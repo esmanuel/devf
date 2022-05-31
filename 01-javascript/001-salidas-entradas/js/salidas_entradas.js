e45b76b04cd173e3c392e825df8ab7024cccc80a
esmanuel <es.manuelbernal@gmail.com>
2022-05-30 21:25:56 -0500

    inicio curso js devf

---
+++ 01-javascript/001-salidas-entradas/js/salidas_entradas.js
@@ -0,0 +1,24 @@
+'use strict'
+window.addEventListener('load', () =>{
+// MOSTRAR DATOS AL USUSARIO console.log()
+console.log('Hola mundo'); // string o texto
+console.log(1991); // numero
+console.log(2+4); // operacion numerica basica
+console.log(10<3); // comparaciones
+
+// document.write REGRESA UN TEXTO EN HTML
+document.write('Hola mundo'); //
+
+//alert() MUESTRA UN POP UP
+alert('Hola mundo');
+    
+// SOLICITA DATOS AL USUARIO FINAL
+prompt('Ingresa tu nombre');
+
+// La ejecucion de las acciones se realiza del parentesis interno hacia afuera
+alert(prompt('Ingresa tu nombre'));
+
+
+
+
+});
