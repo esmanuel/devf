f5716b3ef847708ecb3f9e74a01f63c1737ed599
esmanuel <es.manuelbernal@gmail.com>
2022-06-03 00:51:00 -0500

    Cambios ejercicio vestimente sugerida

--- 01-javascript/003-condicionales/ejercicio-dos/app.js
+++ 01-javascript/003-condicionales/ejercicio-dos/app.js
@@ -37,7 +37,7 @@ let nombre = prompt('¿Como te llamas?');
                     {
                         console.log(decisionOutfitFinal)
                         outfit = document.write(`
-                        <h1>¡Tu eventó será genial ${nombre}! </h1><br>
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
                         <h2>Te recomendamos el siguiente outfit</h2><br>"
                         <img src='https://img.ltwebstatic.com/images3_pi/2021/11/10/1636509285d8ee6f05b42f7c87e10f4319377da9ec_thumbnail_900x.webp'>
                         `)
@@ -45,7 +45,7 @@ let nombre = prompt('¿Como te llamas?');
                     else        //MUJER FROMAL DIA VESTIDO 
                     {
                         outfit = document.write(`
-                        <h1>¡Tu eventó será genial ${nombre}! </h1><br>
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
                         <h2>Te recomendamos el siguiente outfit</h2><br>"
                         <img src='https://img.ltwebstatic.com/images3_pi/2021/01/18/16109432512f73a0fa8c3c0ab9aeb0ea64c765a268_thumbnail_900x.webp'>
                         `)
@@ -61,7 +61,7 @@ let nombre = prompt('¿Como te llamas?');
                         console.log(decisionOutfitFinal)
                         outfit = document.write
                         (`
-                        <h1>¡Tu eventó será genial ${nombre}! </h1><br>
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
                         <h2>Te recomendamos el siguiente outfit</h2><br>"
                         <img src='https://img.ltwebstatic.com/images2_pi/2019/09/10/15681032773990028841_thumbnail_900x1199.webp'>
                         `)
@@ -71,7 +71,7 @@ let nombre = prompt('¿Como te llamas?');
                     {
                         outfit = document.write
                         (`
-                        <h1>¡Tu eventó será genial ${nombre}! </h1><br>
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
                         <h2>Te recomendamos el siguiente outfit</h2><br>"
                         <img src='https://img.ltwebstatic.com/images3_pi/2021/11/17/1637130987e338478a0d95dffbc080db52d476fc32_thumbnail_900x.webp'>
                         `)
@@ -94,7 +94,7 @@ let nombre = prompt('¿Como te llamas?');
                         console.log(decisionOutfitFinal)
                         outfit = document.write
                         (`
-                        <h1>¡Tu eventó será genial ${nombre}! </h1><br>
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
                         <h2>Te recomendamos el siguiente outfit</h2><br>"
                         <img src='https://i.pinimg.com/originals/97/30/92/973092c30779225aa748a986adce5218.jpg'>
                         `);
@@ -105,7 +105,7 @@ let nombre = prompt('¿Como te llamas?');
                     {
                         outfit = document.write
                         (`
-                        <h1>¡Tu eventó será genial ${nombre}! </h1><br>
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
                         <h2>Te recomendamos el siguiente outfit</h2><br>"
                         <img src='https://www.cutypaste.com/wp-content/uploads/2014/09/21.png'>
                         `);
@@ -121,7 +121,7 @@ let nombre = prompt('¿Como te llamas?');
                         console.log(decisionOutfitFinal)
                         outfit = document.write
                         (`
-                        <h1>¡Tu eventó será genial ${nombre}! </h1><br>
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
                         <h2>Te recomendamos el siguiente outfit</h2><br>"
                         <img src='https://www.okchicas.com/wp-content/uploads/2017/10/15-outfits-para-vestir-botas-ensima-de-la-rodillla-1-467x700.jpg'>
                         `);
@@ -132,7 +132,7 @@ let nombre = prompt('¿Como te llamas?');
                     {
                         outfit = document.write
                         (`
-                        <h1>¡Tu eventó será genial ${nombre}! </h1><br>
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
                         <h2>Te recomendamos el siguiente outfit</h2><br>"
                         <img src= 'https://img.ltwebstatic.com/images3_pi/2020/08/15/159746302359a2098258ae7275f7df601cfefad729_thumbnail_900x.webp'>
                         `);
@@ -144,7 +144,126 @@ let nombre = prompt('¿Como te llamas?');
         }
         else            // AQUI COMIENZAN CONDICONALES PARA HOMBRE
         {
-            
+            let evento = prompt('¿Esta vestimenta es para un evento formal? escribe la letra "S"(Si) ó "N"(No)');
+
+            if (evento.toUpperCase(evento.substring(1.1)) === 'S')       //HOMBRE EVENTO FORMAL
+            {
+                console.log(evento)
+                dia_noche = prompt('¿El evento es de día o de noche? escribe la letra "D" (Día) ó "N" (Noche)');
+
+                if (dia_noche.toUpperCase(dia_noche.substring(1.1)) === 'D')    //HOMBRE EVENTO FORMAL DE DIA
+                {
+                    console.log(dia_noche)
+                    decisionOutfitFinal = prompt('¿Prefieres traje o smoking? escribe la letra "T" (Traje) ó "S" (Smoking)');
+
+                    if (decisionOutfitFinal.toUpperCase(decisionOutfitFinal.substring(1.1)) === 'T') //HOMBRE FORMAL DIA TRAJE
+                    {
+                        console.log(decisionOutfitFinal)
+                        outfit = document.write(`
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
+                        <h2>Te recomendamos el siguiente outfit es muy original, serás el alma de la fiesta: </h2><br>"
+                        <img src='https://i.pinimg.com/564x/47/4e/14/474e14895ec578a33d4194f6da7cd615.jpg'>
+                        `)
+                    }
+                    else        //HOMBRE FROMAL DIA SMOKING 
+                    {
+                        outfit = document.write(`
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
+                        <h2>Te recomendamos el siguiente outfit para un evento formal de día, el bigote lo es todo: </h2><br>"
+                        <img src='https://blogs.infobae.com/like/files/2013/08/151.jpg'>
+                        `)
+                        
+                    };
+                }
+                else    //HOMBRE EVENTO FORMAL NOCHE
+                {
+                    decisionOutfitFinal = prompt('¿Prefieres traje o smoking? escribe la letra "T" (Traje) ó "S" (Smoking)');
+
+                    if (decisionOutfitFinal.toUpperCase(decisionOutfitFinal.substring(1.1)) === 'T') //HOMBRE NOCHE TRAJE
+                    {
+                        console.log(decisionOutfitFinal)
+                        outfit = document.write
+                        (`
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
+                        <h2>Te recomendamos el siguiente traje para un evento formal de noche: </h2><br>
+                        <img src='https://scontent.fntr6-4.fna.fbcdn.net/v/t1.6435-9/83448324_641164143356701_2033684495172894720_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=5hN9HynwasIAX-wPA-t&_nc_ht=scontent.fntr6-4.fna&oh=00_AT9QM6aTHc207VVTwwCqKFPkGihzb0dWfZory0dI4Dgr6w&oe=62C10BB9'>
+                        `)
+                        
+                    }
+                    else        //HOMBRE NOCHE SMOKING
+                    {
+                        outfit = document.write
+                        (`
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
+                        <h2>Te recomendamos el siguiente smoking para un evento formal de noche:</h2><br>
+                        <img src='https://i0.wp.com/www.sinembargo.mx/wp-content/uploads/2019/09/1-81.jpg?w=1100&quality=80&strip=all&ssl=1'>
+                        `)
+
+                    };
+                };
+            }
+            else        //MUJER EVENTO CASUAL
+            {
+                console.log(evento)
+                dia_noche = prompt('¿El evento es de día o de noche? escribe la letra "D" (Día) ó "N" (Noche)');
+
+                if (dia_noche.toUpperCase(dia_noche.substring(1.1)) === 'D')    //HOMBRE  CASUAL DIA
+                {
+                    console.log(dia_noche)
+                    decisionOutfitFinal = prompt('¿Prefieres outfit para frío o calor? escribe la letra "F" (Frío) ó "C" (Calor)');
+
+                    if (decisionOutfitFinal.toUpperCase(decisionOutfitFinal.substring(1.1)) === 'F') //HOMBRE CASUAL DIA FRIO
+                    {
+                        console.log(decisionOutfitFinal)
+                        outfit = document.write
+                        (`
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
+                        <h2>Te recomendamos el siguiente outfit para un evento casual, de día y con frío:</h2><br>
+                        <img src='https://static.simpsonswiki.com/images/2/2b/Pie_Man.png'>
+                        `);
+                        
+
+                    }
+                    else        //HOMBRE CASUAL DIA CALOR
+                    {
+                        outfit = document.write
+                        (`
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
+                        <h2>Te recomendamos el siguiente outfit para un evento casual, de día y caluroso: </h2><br>
+                        <img src='https://images2.fanpop.com/images/photos/3000000/Homer-Simpson-homer-simpson-3065105-1024-768.gif'>
+                        `);
+                        console.log(decisionOutfitFinal)
+                    };
+                }
+                else    //HOMBRE EVENTO CASUAL NOCHE
+                {
+                    decisionOutfitFinal = prompt('¿Prefieres outfit para frío o calor? escribe la letra "F" (Frío) ó "C"= (Calor)');
+
+                    if (decisionOutfitFinal.toUpperCase(decisionOutfitFinal.substring(1.1)) === 'F') //HOMBRE CASUAL NOCHE FRIO
+                    {
+                        console.log(decisionOutfitFinal)
+                        outfit = document.write
+                        (`
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
+                        <h2>Te recomendamos el siguiente outfit para un evento casual de noche y con frío: </h2><br>
+                        <img src='https://static.wikia.nocookie.net/simpsons/images/d/de/GABF21_Salamander.jpg/revision/latest?cb=20191026211157'>
+                        `);
+                        
+
+                    }
+                    else        //HOMBRE CASUAL NOCHE CALOR
+                    {
+                        outfit = document.write
+                        (`
+                        <h1>¡Tu evento será genial ${nombre}! </h1><br>
+                        <h2>Te recomendamos el siguiente outfit para un evento casual de noche y caluroso: </h2><br>
+                        <img src= 'https://i.pinimg.com/originals/19/9b/82/199b82ca6a03685c1b9fd90be1deb85d.jpg'>
+                        `);
+                        
+                        console.log(decisionOutfitFinal)
+                    };
+                };
+            };
         };
 
 // }else{
