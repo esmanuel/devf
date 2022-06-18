9ab73a580264036c5b0ad58bcfeef601afdbe03e
esmanuel <es.manuelbernal@gmail.com>
2022-06-18 15:10:26 -0500

    Cajero funcional- detalles y hacerlo responsivo

--- 01-javascript/020-proyecto/cajero/cajero.js
+++ 01-javascript/020-proyecto/cajero/cajero.js
@@ -1,25 +1,25 @@
 /* Crea una aplicación web con JavaScript donde simulemos la interacción con un cajero automático.
-Al ingresar al cajero, puedes seleccionar la cuenta con la que deseas interactuar. Deben existir al menos tres cuentas:
+//-------Al ingresar al cajero, puedes seleccionar la cuenta con la que deseas interactuar. Deben existir al menos tres cuentas:
 Persona 1
 Persona 2
 Persona 3
-Para esto, puedes trabajar con un arreglo de objetos como el siguiente:
+//------Para esto, puedes trabajar con un arreglo de objetos como el siguiente:
 
             var cuentas = [
             { nombre: “Mali”, saldo: 200 }
             { nombre: “Gera”, saldo: 290 }
             { nombre: “Maui”, saldo: 67 }
             ];
-Al seleccionar una cuenta, debes ingresar el password asociado a la cuenta. Si el password es incorrecto, debes notificar al usuario y permitirle intentarlo nuevamente. Si el password es correcto, debes mostrar las siguientes opciones:
+//---------Al seleccionar una cuenta, debes ingresar el password asociado a la cuenta. Si el password es incorrecto, debes notificar al usuario y permitirle intentarlo nuevamente. Si el password es correcto, debes mostrar las siguientes opciones:
 Consultar saldo
 Ingresar monto
 Retirar Monto
 
-Al seleccionar consultar saldo, debe mostrar en pantalla el saldo actual de la cuenta
+-------> Al seleccionar consultar saldo, debe mostrar en pantalla el saldo actual de la cuenta
 
-Al seleccionar ingresar monto, el usuario debe escribir el monto a ingresar. Al ingresar el monto, debe mostrarle al usuario el monto ingresado y el nuevo saldo total.
+//Al seleccionar ingresar monto, el usuario debe escribir el monto a ingresar. Al ingresar el monto, debe mostrarle al usuario el monto ingresado y el nuevo saldo total.
 
-Al seleccionar retirar monto, el usuario debe escribir el monto a retirar. Al retirar el monto, debe mostrarle al usuario el monto retirado y el nuevo saldo total.
+----------->Al seleccionar retirar monto, el usuario debe escribir el monto a retirar. Al retirar el monto, debe mostrarle al usuario el monto retirado y el nuevo saldo total.
 
 Como regla de negocio, una cuenta no debe de tener más de $990 y menos de $10. Es necesario hacer las validaciones pertinentes para que no se rompa esta regla de negocio. 
 */
@@ -28,61 +28,70 @@ Como regla de negocio, una cuenta no debe de tener más de $990 y menos de $10. 
 //retiro si saldo es mayor o igual a 10--- no procede // de lo contrario alerta o mensaje con minimo saldo
 
 //PANELES 
-let inicioDeSesion= document.getElementById('inicioDeSesion');;
-let inicioOperaciones=document.getElementById('inicioOperaciones');;
-let inicioOperacionDeposito=document.getElementById('inicioOperacionDeposito');
-let inicioOperacionRetiro=document.getElementById('inicioOperacionRetiro');
-let inicioOperacionSaldo=document.getElementById('inicioOperacionSaldo');
+let inicioDeSesion = document.getElementById('inicioDeSesion');;
+let inicioOperaciones = document.getElementById('inicioOperaciones');;
+let inicioOperacionDeposito = document.getElementById('inicioOperacionDeposito');
+let inicioOperacionRetiro = document.getElementById('inicioOperacionRetiro');
+let inicioOperacionSaldo = document.getElementById('inicioOperacionSaldo');
 
 let usuario;
 let contrasena;
 let usuarioActivo;
+let index;
+let nuevoSaldo;
+let divHijoSaldo;
+let retiroH2
+let depoH2;
+let texto;
 //como creo una variable con los datos de un usuario que esta activo en sesion?? let usuario activo
 
-const cuentas=[
+const cuentas = [
     {
-        nombre:'Manuel',
-        saldo:300,
-        password:12345678,
+        nombre: 'Manuel',
+        saldo: 300,
+        password: '1234',
     },
     {
-        nombre:'Alejandro',
-        saldo:350,
-        password:12345678,
+        nombre: 'Alejandro',
+        saldo: 350,
+        password: 'qwerty',
     },
     {
-        nombre:'Angelica',
-        saldo:900,
-        password:12345678,
+        nombre: 'Angelica',
+        saldo: 900,
+        password: 'asdf',
     },
 ];
-document.getElementById('iniciarSesion').addEventListener('click', iniciarSesion);
-function iniciarSesion(){
+
+function iniciarSesion() {
     //obtener valores usuario y contraseña
     usuario = document.getElementById('usuario').value;
-    contrasena =document.getElementById('contrasena').value;
+    contrasena = document.getElementById('contrasena').value;
+    console.log(usuario, contrasena);
 
-//Validacion usuario y contraseña
-    if(usuario == cuentas[1].nombre && contrasena == cuentas[1].password){
+    function validador(element) {
+        return element.nombre === usuario;
+    }
+    index = cuentas.findIndex(validador);
+    cuentas[index];
 
+    //Validacion usuario y contraseña
+    if (index === -1) {
+        alert('La contraseña y/o el usuario son incorrectos. Verifique sus datos.')
+    } else if (contrasena !== cuentas[index].password) {
+        alert('La contraseña y/o el usuario son incorrectos. Verifique sus datos.')
+
+    } else {
         inicioDeSesion.classList.add('invisible');
         inicioDeSesion.classList.remove('visible');
 
         inicioOperaciones.classList.add('visible');
         inicioOperaciones.classList.remove('invisible');
-    }else{
-        alert('La contraseña y/o el usuario son incorrectos. Verifique sus datos.')
     }
 };
 
-
-
-
-
-document.getElementById('btn-depositar').addEventListener('click',depositar);
-
-function depositar(){
-    
+//click boton DEPOSITAR
+function depositar() {
 
     inicioOperacionDeposito.classList.add('visible');
     inicioOperacionDeposito.classList.remove('invisible');
@@ -90,37 +99,95 @@ function depositar(){
     inicioOperaciones.classList.add('invisible');
     inicioOperaciones.classList.remove('visible');
 
+    inicioOperacionSaldo.classList.remove('visible');
+    inicioOperacionSaldo.classList.add('invisible');
+
 }
 
-document.getElementById('btn-cantidadDeposito').addEventListener('click',cantidadDeposito);
-function cantidadDeposito(){
-    let valorADepositar=Number(document.getElementById('valorDeposito').value);
-    if((valorADepositar+cuentas[1].saldo)<=990){
-        cuentas[1].saldo=(valorADepositar+cuentas[1].saldo);
+//Click REALIZAR DEPOSITO
+function cantidadDeposito() {
+    let valorADepositar = Number(document.getElementById('valorDeposito').value);
+    if ((cuentas[index].saldo + valorADepositar) > 990) {
+        let maximo = 990 - cuentas[index].saldo;
+        alert(`Solo puedes depositar $${maximo} para tener un max. de $990 en tu cuenta. `);
 
-        inicioOperacionDeposito.classList.add('invisible')
-        inicioOperacionDeposito.classList.remove('visible')
+    } else {
+
+        cuentas[index].saldo = (valorADepositar + cuentas[index].saldo);
+
+        inicioOperacionDeposito.classList.add('invisible');
+        inicioOperacionDeposito.classList.remove('visible');
         inicioOperacionSaldo.classList.remove('invisible');
         inicioOperacionSaldo.classList.add('visible');
 
-        let divHijoSaldo= document.getElementById('hijoSaldo')
-        let H2 = document.createElement('h2');
-        let texto= document.createTextNode(`Depositaste:        $${valorADepositar}`);
-        H2.appendChild(texto);
-        divHijoSaldo.prepend(H2);
+        divHijoSaldo = document.getElementById('hijoSaldo')
+        depoH2 = document.createElement('h2');
+        texto = document.createTextNode(`Depositaste: $${valorADepositar}`);
+        depoH2.appendChild(texto);
+        divHijoSaldo.prepend(depoH2);
 
-        let nuevoSaldo = document.getElementById('resultadoSaldo');
-        nuevoSaldo.value = `$${cuentas[1].saldo}`;
+        nuevoSaldo = document.getElementById('resultadoSaldo');
+        nuevoSaldo.value = `$${cuentas[index].saldo}`;
 
-    }else{
-        let maximo=990-cuentas[1].saldo;
-        alert(`Solo puedes depositar $${maximo} para tener un max. de $990 en tu cuenta. `)
+        divHijoSaldo.removeChild(retiroH2);
     }
 }
 
-document.getElementById('btn-salir').addEventListener('click',salir);
+//click boton RETIRAR
+function retirar() {
 
-function salir(){
+    inicioOperaciones.classList.add('invisible');
+    inicioOperaciones.classList.remove('visible');
+
+    inicioOperacionRetiro.classList.remove('invisible');
+    inicioOperacionRetiro.classList.add('visible');
+
+    inicioOperacionSaldo.classList.remove('visible');
+    inicioOperacionSaldo.classList.add('invisible');
+}
+
+//click REALIZAR RETIRO
+function cantidadRetiro() {
+    let valorARetirar = Number(document.getElementById('numeroRetiro').value);
+    if ((cuentas[index].saldo - valorARetirar) < 10) {
+        let minimo = cuentas[index].saldo - 10;
+        alert(`Solo puedes retirar $${minimo}. Recuerda debes tener un min. de $10 en tu cuenta. `)
+
+    } else {
+
+        cuentas[index].saldo = (cuentas[index].saldo - valorARetirar);
+
+        inicioOperacionRetiro.classList.add('invisible')
+        inicioOperacionRetiro.classList.remove('visible')
+        inicioOperacionSaldo.classList.remove('invisible');
+        inicioOperacionSaldo.classList.add('visible');
+
+        divHijoSaldo = document.getElementById('hijoSaldo')
+        retiroH2 = document.createElement('h2');
+        texto = document.createTextNode(`Retiraste: $${valorARetirar}`);
+        retiroH2.appendChild(texto);
+        divHijoSaldo.prepend(retiroH2);
+
+        nuevoSaldo = document.getElementById('resultadoSaldo');
+        nuevoSaldo.value = `$${cuentas[index].saldo}`;
+
+        divHijoSaldo.removeChild(depoH2);
+    }
+}
+
+//click CONSULTAR SALDO
+function saldo() {
+    inicioOperaciones.classList.add('invisible');
+    inicioOperaciones.classList.remove('visible');
+
+    inicioOperacionSaldo.classList.add('visible');
+    inicioOperacionSaldo.classList.remove('invisible');
+
+    let saldoUsuario = document.getElementById('resultadoSaldo');
+    saldoUsuario.value = `$${cuentas[index].saldo}`;
+}
+//click boton SALIR
+function salir() {
     alert('Estas saliendo de tu sesión. ¡Te esperamos pronto!');
 
     inicioDeSesion.classList.add('visible');
