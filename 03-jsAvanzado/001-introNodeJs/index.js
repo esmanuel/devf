213aa9896a61e82052c8392335f2a91509f880ee
esmanuel <es.manuelbernal@gmail.com>
2022-08-09 11:03:28 -0500

    inicio modulo jsAvanzado

---
+++ 03-jsAvanzado/001-introNodeJs/index.js
@@ -0,0 +1,12 @@
+console.log('estoy escribiendo en terminal');
+var colors = require('colors')
+
+function Sumar(x,y){
+	let suma = x+y;
+	console.log('La suma es: '.red + suma);
+};
+Sumar(30,40);
+
+for(let i = 0; i<5; i++){
+	console.log('Manuel'.blue)
+}
